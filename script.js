c5e36c167665e5b78016861828cbeaf3614953f0
Isanka Rathnayaka <38767323+IshiTinkerBell@users.noreply.github.com>
2018-05-02 13:10:45 +0530

    Removed the new changes

--- script.js
+++ script.js
@@ -7,29 +7,5 @@ var app = new Vue({
       }
     });
 
-    function createPerson()
-    {
-      return person = {'Name':'','Age':'','Address':''};  
-    }
+   
     
-    var objectArray = [];
-    var person1 = createPerson();
-    person1.Name = "Scott";
-    person1.Age = "12";
-    person1.Address = "Home";
-    objectArray.push(person1);
-    
-    var person2 = createPerson();
-    person2.Name = "Jane";
-    person2.Age = "14";
-    person2.Address = "Work";
-    objectArray.push(person2);
-    
-    
-      var mainWrapper = new Vue({
-
-        el: '#mainWrapper',
-        data: {
-          content: objectArray
-        }
-      })
